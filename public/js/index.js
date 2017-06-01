bad5d9797ea76459436ab832ed9092038ef8a417
jli0423 <justin.li0423@gmail.com>
2017-06-01 15:15:40 -0400

    emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function(){
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: 'justin',
-    text: 'this is meeee'
-  });
 });
 //check for disconnect
 socket.on('disconnect', function(){
--- server/server.js
+++ server/server.js
@@ -19,14 +19,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'asdfasdfasd',
-    createAt: 1232
-  });
-
-  socket.on('createMessage', (Message) =>{
-    console.log('createMessage', Message);
+  socket.on('createMessage', (message) =>{
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', ()=>{
